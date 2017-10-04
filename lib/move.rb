5d1e670d7413d84e04569067f31f752383b52fdd
Derek Haller <dhaller.personal@gmail.com>
2017-10-04 21:19:49 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -6,4 +6,9 @@ def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board[pos - 1] = "#{pos}"
+def get_index(pos)
+  index = pos - 1
+end
+
+def move(board, index, value = "X")
+  board[index] = "#{pos}"
